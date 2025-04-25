55d833520f552be6ab70b28fae610c8be8e4e9ca
Andrew Plotkin <zarf@ZarfLent.local>
2025-04-24 22:14:37 -0400

    More tweak.

--- tweak-for-test-mode.sh
+++ tweak-for-test-mode.sh
@@ -6,5 +6,11 @@
 
 cd /var/ifarchive
 
+# Turn on testmode in the darkmode.js script. (This lets the theme cookie
+# work outside the ifarchive.org domain.)
 sed -i 's/const testmode = false/const testmode = true/' htdocs/misc/darkmode.js
 
+# Adjust front-page links to be server-relative rather than pointing at
+# production domains.
+sed -i 's,https://search.ifarchive.org,,g' htdocs/index.html
+sed -i 's,https://upload.ifarchive.org,,g' htdocs/index.html

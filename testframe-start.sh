808e638132c7413eafb4d8a961907273efd42157
Andrew Plotkin <zarf@ZarfLent.local>
2025-04-26 19:37:50 -0400

    Initialize conditional.

--- testframe-start.sh
+++ testframe-start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -e /var/ifarchive/lib/testframe-started ]; then
+    # already set up
+    echo
+else
+    echo 'Initing testframe...'
+    touch /var/ifarchive/lib/testframe-started
+fi
+
 echo 'Starting testframe...'
 
 # Run Apache (with logs on stdout) as our front container process.

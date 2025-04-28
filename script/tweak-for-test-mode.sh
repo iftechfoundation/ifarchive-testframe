6111d5c1d75fe64bb27a97bcab314f3b20a697d7
Andrew Plotkin <zarf@ZarfLent.local>
2025-04-28 16:14:36 -0400

    Remove one unneeded tweak.

--- script/tweak-for-test-mode.sh
+++ script/tweak-for-test-mode.sh
@@ -6,10 +6,6 @@
 
 cd /var/ifarchive
 
-# Turn on testmode in the darkmode.js script. (This lets the theme cookie
-# work outside the ifarchive.org domain.)
-sed -i 's/const testmode = false/const testmode = true/' htdocs/misc/darkmode.js
-
 # Adjust front-page links to be server-relative rather than pointing at
 # production domains.
 sed -i 's,https://search.ifarchive.org,,g' htdocs/index.html
